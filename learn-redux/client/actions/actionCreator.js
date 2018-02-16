177dd382dc1dd563948758c4d52da613655d2e7f
Mario Aguiar <mario@10up.com>
2018-02-16 10:48:02 -0600

    Create action creators

---
+++ learn-redux/client/actions/actionCreator.js
@@ -0,0 +1,27 @@
+// Increment like
+export function increment( index ) {
+	return {
+		type: 'INCREMENT_LIKES',
+		index
+	}
+}
+
+// Add comment
+export function add_comment( postId, author, comment ) {
+	return {
+		type: 'ADD_COMMENT',
+		postId,
+		author,
+		comment
+	}
+}
+
+// Remove comment
+
+export function remove_comment( postId, index ) {
+	return {
+		type: 'REMOVE_COMMENT',
+		index,
+		postId
+	}
+}
