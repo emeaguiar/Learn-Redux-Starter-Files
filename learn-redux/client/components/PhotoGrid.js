73dec673b866925cffa69a16d64963fab0f59889
Mario Aguiar <mario@10up.com>
2018-02-16 14:20:31 -0600

    Actually display post cards

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 class PhotoGrid extends Component {
 	render() {
 		return  (
 			<div className="photo-grid">
-				I'm the photo grid
+				{ this.props.posts.map( ( post, index ) => <Photo { ...this.props } key={ `photo-${ index }` } id={ index } post={ post } /> ) }
 			</div>
 		);
 	}
